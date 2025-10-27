58838082b436b032d6ef24b29ff262893cba6a31
Samuel <samuelop3308@gmail.com>
2025-10-26 23:13:54 -0500

    admin x2

--- ProyectoFinal/src/co/edu/unbosque/model/Administrador.java
+++ ProyectoFinal/src/co/edu/unbosque/model/Administrador.java
@@ -3,29 +3,18 @@ package co.edu.unbosque.model;
 import java.io.Serializable;
 import java.time.LocalDate;
 
-public class Administrador extends Usuario implements Serializable{
+public class Administrador extends Persona implements Serializable{
 
 	public Administrador() {
 
 	}
 
 	public Administrador(String nombre, String apellido, String email, LocalDate fecha, String genero,
-			Boolean esAdministrador, Boolean estaDisponible, String alias, String foto, Boolean esIncognito,
-			int numLikes) {
-		super(nombre, apellido, email, fecha, genero, esAdministrador, estaDisponible, alias, foto, esIncognito,
-				numLikes);
-	}
-
-	public Administrador(String nombre, String apellido, String email, LocalDate fecha, String genero,
 			Boolean esAdministrador) {
 		super(nombre, apellido, email, fecha, genero, esAdministrador);
-		// TODO Auto-generated constructor stub
 	}
-	
-	public Administrador(Boolean estaDisponible, String alias, String foto, Boolean esIncognito, int numLikes) {
-		super(estaDisponible, alias, foto, esIncognito, numLikes);
-		// TODO Auto-generated constructor stub
-	}
+
+
 
 	
 	
